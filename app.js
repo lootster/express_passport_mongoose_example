140d432150730e1e7814c2150e8df27f67d3b3aa
songguoqiang <songguoq@gmail.com>
2018-10-19 15:18:29 +0800

    remove unused package expression-session

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const express = require("express"),
   bodyParser = require("body-parser"),
-  session = require("express-session"),
   cors = require("cors"),
   errorhandler = require("errorhandler"),
   mongoose = require("mongoose");
@@ -21,15 +20,6 @@ app.use(bodyParser.json());
 
 app.use(require("method-override")());
 app.use(express.static(__dirname + "/public"));
-
-app.use(
-  session({
-    secret: "conduit",
-    cookie: { maxAge: 60000 },
-    resave: false,
-    saveUninitialized: false
-  })
-);
 
 if (!isProduction) {
   app.use(errorhandler());
